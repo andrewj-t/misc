ebcc2713bb331632b76e405cf49120de5fb7fab9
andrewj-t <67892040+andrewj-t@users.noreply.github.com>
2025-10-05 21:40:46 +0800

    npp

--- ServerFirstRun.ps1
+++ ServerFirstRun.ps1
@@ -1,3 +1,4 @@
+# Set Policies to make Edge more suitable for server use
 $EdgePolicyPath = 'HKLM:\SOFTWARE\Policies\Microsoft\Edge'
 if (-not (Test-Path $EdgePolicyPath)) { New-Item -Path $EdgePolicyPath -Force | Out-Null }
 
@@ -17,7 +18,7 @@ Set-ItemProperty -Path $EdgePolicyPath -Name 'NewTabPageContentEnabled' -Type DW
 Set-ItemProperty -Path $EdgePolicyPath -Name 'NewTabPageLocation' -Type String -Value 'about:blank'
 
 
-
+# Disable Server Manager pop-up at logon and launch
 if (-not (Test-Path 'HKLM:\SOFTWARE\Microsoft\ServerManager')) {
     New-Item -Path 'HKLM:\SOFTWARE\Microsoft\ServerManager' | Out-Null
 }
@@ -28,3 +29,21 @@ if (-not (Test-Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\Server\ServerMana
 }
 Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\Server\ServerManager' -Name 'DoNotOpenAtLogon' -Value 1 -Type DWord
 
+# Install Evergreen and Notpad++
+if (Get-PSRepository | Where-Object { $_.Name -eq "PSGallery" -and $_.InstallationPolicy -ne "Trusted" }) {
+    Install-PackageProvider -Name "NuGet" -MinimumVersion 2.8.5.208 -Force
+    Set-PSRepository -Name "PSGallery" -InstallationPolicy "Trusted"
+}
+
+Install-Module -Name Evergreen
+Import-Module -Name Evergreen
+
+
+Get-EvergreenApp -Name "NotepadPlusPlus"
+
+$Npp = Get-EvergreenApp -Name NotepadPlusPlus | Where-Object { $_.Architecture -eq "x64" -and $_.InstallerType -eq "Default" }
+$NppInstaller = Split-Path -Path $Npp.Uri -Leaf
+$InstallerPath = $Env:TEMP + "\" + $NppInstaller
+Invoke-WebRequest -Uri $Npp.Uri -OutFile $InstallerPath -UseBasicParsing
+
+Start-Process -FilePath $InstallerPath -ArgumentList "/S" -Wait
